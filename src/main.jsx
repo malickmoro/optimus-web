ea24a65c4e6082fb52607f921eda7337ec3d92f2
Young Plutus <malicksamah3@gmail.com>
2025-08-30 05:05:00 +0000

    litecoin validation fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Buffer } from "buffer";
 import App from "./App.jsx";
 import { Analytics } from "@vercel/analytics/react"
 import { ContextVariablesProvider } from "./context/ContextVariables.jsx";
@@ -18,3 +19,4 @@ if (rootElement) {
   </StrictMode>
   );
 }
+window.Buffer = Buffer;
